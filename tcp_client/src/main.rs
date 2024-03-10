db6e34576c03235a001f90dfc2d19765bb02246e
Maxime Emonnot <61348720+MaximeEmonnot@users.noreply.github.com>
2024-03-10 21:27:32 +0100

    Fonctions client déplacées dans net.rs

--- tcp_client/src/main.rs
+++ tcp_client/src/main.rs
@@ -6,30 +6,17 @@ mod ui;
 fn main() {
     println!("Tentative de connexion au serveur...");
 
-    let mut stream = TcpStream::connect("127.0.0.1:1234").expect("Erreur : Impossible de se connecter au serveur distant");
-    stream.set_nonblocking(false).expect("Erreur : Impossible de rendre le socket non-bloquant");
+    let mut stream = net::connect("127.0.0.1:1234", false);
+    let mut client = stream.try_clone().expect("Erreur : Impossible de cloner le socket");
 
-    let mut client = stream.try_clone().expect("Error cloning stream");
-    // Read
     thread::spawn(move || loop {
-        let mut data = [0 as u8; 2048];
-        match stream.read(&mut data) {
-            Ok(_) => {
-                let msg = from_utf8(&data).unwrap();
-                println!("Message recieved : {}", msg);
-            }
-            Err(e) => {
-                println!("Failed to recieve data : {}", e);
-            }
-        }
-    }); 
-
-    // Write
+        let msg = net::handle_read(&mut stream);
+        println!("Recieved : {}", msg);
+    });
+    
     loop {
         let mut buff = String::new();
         io::stdin().read_line(&mut buff).expect("Reading from stdin failed");
-
-        let msg = buff.trim().to_string();
-        client.write(msg.as_bytes());
+        net::handle_write(&mut client, &buff);
     }
 }
--- tcp_client/src/net.rs
+++ tcp_client/src/net.rs
@@ -1,72 +1,24 @@
-use std::{io::{stdin, Read, Write}, net::TcpStream, sync::mpsc};
+use std::{io::{Read, Write}, net::TcpStream};
 
-/*
- * Objectif de la classe : 
- * - Connexion à un serveur via un socket
- * - Permettre la lecture des données provenant du serveur à tout moment
- * - Permettre l'envoi des données à tout moment
- */
-/*
-struct TcpSocket {
-    stream: Option<TcpStream>,
-    address: String,
+pub fn connect(address : &str, non_blocking : bool) -> TcpStream {
+    let mut stream = TcpStream::connect(address).expect("Erreur : Impossible de se connecter au serveur distant");
+    stream.set_nonblocking(non_blocking).expect("Erreur : Impossible de modifier l'aspect non-bloquant");
+    return stream;
 }
 
-impl TcpSocket {
-    fn new() -> Self
-    {
-        Self {
-            stream: None,
-            address: "".to_string()
+pub fn handle_read(stream : &mut TcpStream) -> String {
+    let mut data = [0 as u8; 2048];
+    match stream.read(&mut data) {
+        Ok(_) => {
+            return String::from_utf8((&data).to_vec()).unwrap();
+        }
+        Err(e) => {
+            println!("Erreur : Impossible de récupérer les données : {}", e);
         }
     }
-    
-    fn connect(&mut self, address: &String)
-    {
-        self.address = address.clone();
-        self.stream = TcpStream::connect(address).expect("Erreur : Impossible de se connecter au serveur distant");
-    }
-    
-    fn set_nonblocking(&mut self, value: bool)
-    {
-        self.stream.set_nonblocking(value).expect("Erreur : Impossible rendre le socket non-bloquant");
-    }
-    
-    fn handle_write(&mut self, msg : &String)
-    {
-        self.stream.write(msg.as_bytes()).unwrap();
-        
-        println!("Message envoyé avec succès ! : {}", msg);
-    }
-    
-    fn handle_read(&mut self) -> String
-    {
-        let mut data = [0 as u8; 6];
-        self.stream.read_exact(&mut data).expect("Erreur : Perte de connexion au serveur");
-        return String::from_utf8(data.to_vec()).expect("Erreur : Impossible de convertir en String");
-    }
+    return String::new();
 }
 
-pub struct Client {
-    socket : TcpSocket,
-    name : String,
+pub fn handle_write(stream : &mut TcpStream, msg : &String) {
+    stream.write(msg.trim().as_bytes());
 }
-
-impl Client {
-    pub fn new(name: String) -> Self
-    {
-        Self{
-            socket: TcpSocket::new(),
-            name: name
-        }
-    }
-    
-    pub fn run(&self, address: &String)
-    {
-        self.socket.connect(address);
-        self.socket.set_nonblocking(true);
-        
-        self.socket.handle_write(&"Gros PD".to_string());
-    }
-}
-*/
